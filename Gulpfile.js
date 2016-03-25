2d5049f905117452855b7329b58d4b7be9cac22a
Janne Niutanen <janne.niutanen@solita.fi>
2016-03-25 08:44:17 +0200

    Moved elm and server sources to same level. Added Procfile for Heroku

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var elm  = require('gulp-elm');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var gulputil = require('gulp-util');
+
+gulp.task('elm-init', elm.init);
+
+gulp.task('elm', ['elm-init'], function(){
+    return gulp.src('elm/App.elm')
+        .pipe(elm().on('error', gulputil.log))
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('watch', function (done) {
+    watch('elm/**/*.elm', function () {
+        gulp.start(['elm']);
+    });
+});
+
+gulp.task('default', ['elm', 'watch']);
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm install && gulp elm && npm start
